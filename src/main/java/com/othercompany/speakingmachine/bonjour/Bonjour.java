2da98550dc2b9f49b044fd55b51cc050b6ab06d8
WANG <wangqianlf@live.com>
2024-05-14 15:00:53 +0200

    premier commit, ajout .gitigore

---
+++ src/main/java/com/othercompany/speakingmachine/bonjour/Bonjour.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.othercompany.speakingmachine.bonjour;
+
+/**
+ *
+ * @author wangq
+ */
+public class Bonjour {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
